d5e47622b8017b3e2db7a440e72daf125127791c
myyy505 <helmiafandi25@gmail.com>
2025-04-17 22:17:47 +0700

    update

---
+++ week6/Palindrome.java
@@ -0,0 +1,26 @@
+package week6;
+
+import java.util.Scanner;
+// Kelas Main (program utama)
+public class Palindrome {
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+
+      System.out.println("=========== Palindrome ===========");
+      System.out.println("===== Ahmad Helmi Afandi =========");
+      String again = "y";
+
+      while(again.equalsIgnoreCase("y")){
+        System.out.print("Masukkan kata: ");
+        String input = scan.nextLine();
+        TextGame game1 = new TextGame();
+        TextGame game2 = new FancyTextGame();
+        game1.displayResult(input);
+        game2.displayResult(input);
+        System.out.print("Apakah Anda ingin mencoba lagi? (y/n): ");
+        again = scan.nextLine(); 
+        System.out.println();
+      }
+
+    }
+}
